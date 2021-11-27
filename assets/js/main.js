da9fe9fb08b023e66114edbf24c2c6efc37dfbe0
wahidari <wahiid.ari@gmail.com>
2021-11-27 17:57:26 +0700

    add video

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,14 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
+// Preloader
+setTimeout(function () {
+  if ($("#preloader").length > 0) {
+    $("#preloader").remove();
+  }
+}, 2000);
+
+
 (function () {
   ("use strict");
 
@@ -97,46 +105,7 @@ function backToTop() {
     }
   });
 
-  /**
-   * Scrolls to an element with header offset
-   */
-  // const scrollto = (el) => {
-  //   let header = select("#header");
-  //   let offset = header.offsetHeight;
-
-  //   if (!header.classList.contains("header-scrolled")) {
-  //     offset -= 10;
-  //   }
-
-  //   let elementPos = select(el).offsetTop;
-  //   window.scrollTo({
-  //     top: elementPos - offset,
-  //     behavior: "smooth",
-  //   });
-  // };
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  // on(
-  //   "click",
-  //   ".scrollto",
-  //   function (e) {
-  //     if (select(this.hash)) {
-  //       e.preventDefault();
-
-  //       let navbar = select("#navbar");
-  //       if (navbar.classList.contains("navbar-mobile")) {
-  //         navbar.classList.remove("navbar-mobile");
-  //         let navbarToggle = select(".mobile-nav-toggle");
-  //         navbarToggle.classList.toggle("bi-list");
-  //         navbarToggle.classList.toggle("bi-x");
-  //       }
-  //       scrollto(this.hash);
-  //     }
-  //   },
-  //   true
-  // );
+  
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
@@ -162,4 +131,5 @@ function backToTop() {
     this.classList.toggle("bi-list");
     this.classList.toggle("bi-x");
   });
-})();;
+})();
+
