b6ffa581ca83f95f44228266faa673062d3d529e
wahidari <wahiid.ari@gmail.com>
2021-11-27 10:44:38 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,47 @@ function backToTop() {
   };
 
   /**
+   * Scrolls to an element with header offset
+   */
+  // const scrollto = (el) => {
+  //   let header = select("#header");
+  //   let offset = header.offsetHeight;
+
+  //   if (!header.classList.contains("header-scrolled")) {
+  //     offset -= 10;
+  //   }
+
+  //   let elementPos = select(el).offsetTop;
+  //   window.scrollTo({
+  //     top: elementPos - offset,
+  //     behavior: "smooth",
+  //   });
+  // };
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  // on(
+  //   "click",
+  //   ".scrollto",
+  //   function (e) {
+  //     if (select(this.hash)) {
+  //       e.preventDefault();
+
+  //       let navbar = select("#navbar");
+  //       if (navbar.classList.contains("navbar-mobile")) {
+  //         navbar.classList.remove("navbar-mobile");
+  //         let navbarToggle = select(".mobile-nav-toggle");
+  //         navbarToggle.classList.toggle("bi-list");
+  //         navbarToggle.classList.toggle("bi-x");
+  //       }
+  //       scrollto(this.hash);
+  //     }
+  //   },
+  //   true
+  // );
+
+  /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
   let selectHeader = select("#header");
